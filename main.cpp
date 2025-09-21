2c9d66f73b2d6da150e99602eaed593fb0826565
Alex Belofastov <a.belofastov@mail.ru>
2025-09-21 17:59:41 +0000

    FIX

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@
 int main() {
     int hour, minute;
     std::string period;
-    std::cout << "Enter time (hour minute period(am/pm)): ";
     std::cin >> hour >> minute >> period;
     std::string converted_time = time_converter(hour, minute, period);
-    std::cout << "Converted time (24-hour format): " << converted_time << std::endl;
+    std::cout << "Converted time" << converted_time << std::endl;
     return 0;
 }
